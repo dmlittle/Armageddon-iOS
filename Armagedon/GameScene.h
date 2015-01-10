47cb4778292885d8f795a8367b66d2e73e4f72ca
Donald Little <dmlittle@mit.edu>
2015-01-10 15:09:55 -0500

    Initial Commit

---
+++ Armagedon/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Armagedon
+//
+
+//  Copyright (c) 2015 Little Games. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Armagedon/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Armagedon
+//
+
+//  Copyright (c) 2015 Little Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
